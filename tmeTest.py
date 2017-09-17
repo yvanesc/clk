03997f0f7b8fe8ca309574885c70b60050ce65a1
yvanesc <yvan.esc@gmail.com>
2017-09-17 02:32:55 +0200

    time test

---
+++ tmeTest.py
@@ -0,0 +1,10 @@
+import time
+from time import localtime, strftime
+print(strftime("%a, %d %b %Y %H:%M:%S +0000", localtime()))
+print(strftime("%H", localtime())[:1])
+print(strftime("%H", localtime())[1:])
+strH1 = strftime("%H", localtime())[:1]
+strH2 = strftime("%H", localtime())[1:]
+strM1 = strftime("%H", localtime())[:1]
+strM1 = strftime("%H", localtime())[1:]
+if strH1 == "1": print("OK")
