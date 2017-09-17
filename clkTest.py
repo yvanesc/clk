12e43745660f2e39ee641cc83b7df39b899c5a4a
yvanesc <yvan.esc@gmail.com>
2017-09-17 02:17:44 +0200

    time + img done

--- clkTest.py
+++ clkTest.py
@@ -1,15 +1,21 @@
 import pygame
 import os
 import sys
-
+import time
+from time import localtime, strftime
 pygame.init()
 
+#screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 screen = pygame.display.set_mode((320, 240))
 caption = pygame.display.set_caption("Digital Clock") 
 
 screen = pygame.display.get_surface()
-
-imgH1 = os.path.join("/home/pi/Documents/clock/1.bmp")
+#--------------time
+#print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+strH2 = strftime("%H", localtime())[1:]
+strH2 = strH2 + ".bmp"
+#imgH1 = os.path.join("/home/pi/Documents/clkTest/nb/1.bmp")
+imgH1 = os.path.join("/home/pi/Documents/clkTest/nb/"+ strH2)
 imgH2 = os.path.join("/home/pi/Documents/clock/2.bmp")
 imgM1 = os.path.join("/home/pi/Documents/clock/2.bmp")
 imgM2 = os.path.join("/home/pi/Documents/clock/1.bmp")
@@ -21,6 +27,7 @@ imgM2 = pygame.image.load(imgM2).convert()
 screen.blit(imgH1, (0, 0))
 screen.blit(imgH2, (80, 0))
 screen.blit(imgM1, (160, 0))
-screen.blit(imgM2, (240, 0))
+screen.blit(imgM2, (660,0)) #(240, 0))
 
 pygame.display.update()
+time.sleep(10)
